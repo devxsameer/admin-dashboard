9d5d60b1ca93048d73d83d7451cfe22376ab7e96
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2025-07-04 11:30:24 +0530

    feat: :sparkles: Complete Design

---
+++ script.js
@@ -0,0 +1,16 @@
+const navBar = document.querySelector(".navbar"),
+  toggle = document.querySelector(".toggle"),
+  modeSwitch = document.querySelector(".toggle-switch"),
+  menuLinks = document.querySelectorAll(".menu-link");
+
+toggle.addEventListener("click", () => {
+  navBar.classList.toggle("open");
+});
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navBar.classList.add("open");
+  });
+});
+modeSwitch.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark");
+});
